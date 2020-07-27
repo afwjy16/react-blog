5cdd3b8a4950860668125e8131e055439dd7e695
afwjy16 <afif0071997@gmail.com>
2020-07-27 15:17:17 +0700

    model user

--- model/user.js
+++ model/user.js
@@ -9,6 +9,29 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
     }
-
 })
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+}
